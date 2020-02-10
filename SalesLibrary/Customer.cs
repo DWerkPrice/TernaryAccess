19495b57d693729dc935d6f28d9105cdca3580b0
David Price <dwerkprice@gmail.com>
2020-02-10 16:30:25 -0500

    second commit in afternoon

---
+++ SalesLibrary/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TernaryAccess
+{
+    public class Customer {
+
+        private static int NextId = 1;
+        public int Id { get; private set; } // private before set prevents people from changing id
+        public string Name { get; set; }
+        private bool IsNationalAccount { get; set; }
+
+        public void NationalAccount(string yesOrNo) {
+            this.IsNationalAccount = yesOrNo.Equals("yes") ? true : false;
+        }
+
+        public static string PrintNextId() {
+            return $"Next id is {NextId}";
+        }
+        public Customer(string Name) {
+            this.Id = NextId;
+            this.Name = Name;
+            NextId++;
+        }
+    }
+}
--- TernaryAccess/Program.cs
+++ TernaryAccess/Program.cs
@@ -2,13 +2,25 @@
 
 namespace TernaryAccess
 {
-    class Program
-    {
+    class Program {
         static void Main(string[] args) {
-            var cust1 = new Customer { Id = 1 , Name = "Max Technical Training" };
+            Console.WriteLine($" The cube of 17 is {Program.Cubed(17)}");
+            //          var message = Customer.PrintNextId();
+            //          Console.WriteLine(message);
+            Console.WriteLine(Customer.PrintNextId());
+            
+            var cust1 = new Customer("Max Technical Training");
+            var cust2 = new Customer("Amazon");
+            var cust3 = new Customer("Microsoft");
+
             cust1.NationalAccount("yes");
 
-            var oorder1 = new Order { Id = 1 , Amount = 1000 , Customer = cust1 };
+
+            var order1 = new Order { Id = 1 , Amount = 1000 , Customer = cust1 };
+
+        }
+        static int Cubed(int nbr) {
+            return nbr * nbr * nbr;
 
         }
     }
